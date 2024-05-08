8ad3e69d56fdb5fb523e60f5e2030897d0bb4f7b
pooja <poojaskumara@gmail.com>
2024-05-08 17:55:14 +0530

    start expense project files

--- frontend.sh
+++ frontend.sh
@@ -1,29 +1,29 @@
 source common.sh
-rm -f /temp/expense.log
+rm -f /tmp/expense.log
 
 Heading installing nginx
-dnf install nginx -y &>>/temp/expense.log
+dnf install nginx -y &>>/tmp/expense.log
 STAT $?
 
 Heading copy the expense.config file
-cp expense.config /etc/nginx/default.d/expense.conf &>>/temp/expense.log
+cp expense.config /etc/nginx/default.d/expense.conf &>>/tmp/expense.log
 STAT $?
 
 Heading clean old content
-rm -rf /usr/share/nginx/html/*  &>>/temp/expense.log
+rm -rf /usr/share/nginx/html/*  &>>/tmp/expense.log
 STAT $?
 
 
 Heading download frontend content
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip &>>/temp/expense.log
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip &>>/tmp/expense.log
 STAT $?
 cd /usr/share/nginx/htmlecho
 
 Heading extract the content
-unzip /tmp/frontend.zip. &>>/temp/expense.log
+unzip /tmp/frontend.zip. &>>/tmp/expense.log
 STAT $?
 
 Heading restart content
-systemctl enable &>>/temp/expense.log
-systemctl restart nginx &>>/temp/expense.log
+systemctl enable &>>/tmp/expense.log
+systemctl restart nginx &>>/tmp/expense.log
 STAT $?
