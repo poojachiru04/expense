05c6b9bcbd4c03224f3b3805a6022ac863e51739
pooja <poojaskumara@gmail.com>
2024-05-08 18:07:03 +0530

    start expense project files

--- mysql.sh
+++ mysql.sh
@@ -1,20 +1,20 @@
 source common.sh
-rf -f /temp/expense.log
+rf -f /tmp/expense.log
 
-if [-z "$1"]; then
+if [ -z "$1" ]; then
   echo "Input mysql password is missing"
   exit 1
 fi
 
 Heading Installing mysql server
-dnf install mysql-server -y &>>/temp/expense.log
+dnf install mysql-server -y &>>/tmp/expense.log
 STAT $?
 
 Heading start the nginx server
-systemctl enable mysqld &>>/temp/expense.log
-systemctl start mysqld &>>/temp/expense.log
+systemctl enable mysqld &>>/tmp/expense.log
+systemctl start mysqld &>>/tmp/expense.log
 STAT $?
 
 Heading set root password
-mysql_secure_installation --set-root-pass $1 &>>/temp/expense.log
+mysql_secure_installation --set-root-pass $1 &>>/tmp/expense.log
 STAT $?
